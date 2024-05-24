207759984574595833cb213f9388d9c2d84f97d0
jst408.hz <jst408.hz@gmail.co>
2024-05-24 21:36:35 +0800

    feat: add build

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,16 +1,3 @@
 import comp from "/Users/sunjunjie/projects/cbtgeek.static/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"Hello VuePress\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1715613389000,\"contributors\":[{\"name\":\"sunjunjie\",\"email\":\"jessehain@foxmail.com\",\"commits\":4}]},\"filePathRelative\":\"README.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"Hello VuePress\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1716557540000,\"contributors\":[{\"name\":\"sunjunjie\",\"email\":\"jessehain@foxmail.com\",\"commits\":4},{\"name\":\"jst408.hz\",\"email\":\"jst408.hz@gmail.co\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
