5ddb4a18230b90174ac89afaef60327c9928c549
jst408.hz <jst408.hz@gmail.co>
2024-05-26 16:34:12 +0800

    feat: update

--- src/components/PageLayout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <PageHeader></PageHeader>
-    <div class="page-content">
-        <slot></slot>
-    </div>
-</template>
-<script setup>
-import PageHeader from '@/components/PageHeader/index.vue';
-</script>
--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,8 +1,13 @@
 <template>
-    <p>main layout</p>
-    <Content></Content>
-    <PageHeader></PageHeader>
+    <div class="min-h-screen">
+        <PageHeader></PageHeader>
+        <div class="container mx-auto">
+            <Content></Content>
+        </div>
+        <PageFooter></PageFooter>
+    </div>
 </template>
 <script setup>
 import PageHeader from '@/components/PageHeader/index.vue';
+import PageFooter from '@/components/PageFooter/index.vue';
 </script>
