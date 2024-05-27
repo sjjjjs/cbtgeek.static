3e8073c0012bfa9b5f91817a412e62a8a3b29a23
jst408.hz <jst408.hz@gmail.co>
2024-05-27 14:55:49 +0800

    feat: update

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -4,9 +4,9 @@ export const routes = Object.fromEntries([
   ["/", { loader: () => import(/* webpackChunkName: "index.html" */"/Users/sunjunjie/projects/cbtgeek.static/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":""} }],
   ["/about.html", { loader: () => import(/* webpackChunkName: "about.html" */"/Users/sunjunjie/projects/cbtgeek.static/docs/.vuepress/.temp/pages/about.html.js"), meta: {"title":"About CBT Geek"} }],
   ["/privacy-policy.html", { loader: () => import(/* webpackChunkName: "privacy-policy.html" */"/Users/sunjunjie/projects/cbtgeek.static/docs/.vuepress/.temp/pages/privacy-policy.html.js"), meta: {"title":"PRIVACY POLICY"} }],
+  ["/post/", { loader: () => import(/* webpackChunkName: "post_index.html" */"/Users/sunjunjie/projects/cbtgeek.static/docs/.vuepress/.temp/pages/post/index.html.js"), meta: {"title":""} }],
   ["/product/", { loader: () => import(/* webpackChunkName: "product_index.html" */"/Users/sunjunjie/projects/cbtgeek.static/docs/.vuepress/.temp/pages/product/index.html.js"), meta: {"title":""} }],
   ["/product/Wallbox%20Station%20Home%20EV%20Chargers.html", { loader: () => import(/* webpackChunkName: "product_Wallbox Station Home EV Chargers.html" */"/Users/sunjunjie/projects/cbtgeek.static/docs/.vuepress/.temp/pages/product/Wallbox Station Home EV Chargers.html.js"), meta: {"title":"Wallbox Station Home EV Chargers"} }],
-  ["/post/", { loader: () => import(/* webpackChunkName: "post_index.html" */"/Users/sunjunjie/projects/cbtgeek.static/docs/.vuepress/.temp/pages/post/index.html.js"), meta: {"title":""} }],
   ["/post/blog/Mode%202%20Portable%20Ev%20Charger%20FAQ.html", { loader: () => import(/* webpackChunkName: "post_blog_Mode 2 Portable Ev Charger FAQ.html" */"/Users/sunjunjie/projects/cbtgeek.static/docs/.vuepress/.temp/pages/post/blog/Mode 2 Portable Ev Charger FAQ.html.js"), meta: {"title":"Mode 2 Portable Ev Charger FAQ"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/Users/sunjunjie/projects/cbtgeek.static/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
