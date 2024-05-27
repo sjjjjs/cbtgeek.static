a8c235cdae1b46a3498a3d4822b979acbdabbc4c
jst408.hz <jst408.hz@gmail.co>
2024-05-27 14:23:29 +0800

    feat: update

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -10,3 +10,20 @@ export const routes = Object.fromEntries([
   ["/post/blog/Mode%202%20Portable%20Ev%20Charger%20FAQ.html", { loader: () => import(/* webpackChunkName: "post_blog_Mode 2 Portable Ev Charger FAQ.html" */"/Users/sunjunjie/projects/cbtgeek.static/docs/.vuepress/.temp/pages/post/blog/Mode 2 Portable Ev Charger FAQ.html.js"), meta: {"title":"Mode 2 Portable Ev Charger FAQ"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/Users/sunjunjie/projects/cbtgeek.static/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateRoutes) {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+  }
+  if (__VUE_HMR_RUNTIME__.updateRedirects) {
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ routes, redirects }) => {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  })
+}
