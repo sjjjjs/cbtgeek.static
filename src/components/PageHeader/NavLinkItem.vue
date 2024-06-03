6f43565f59423f4d9d018df22ce4b704cb9bd7cf
jst408.hz <jst408.hz@gmail.co>
2024-06-03 13:27:18 +0800

    feat: update

---
+++ src/components/PageHeader/NavLinkItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <a v-if="!$slots.default" class="btn btn-ghost" v-bind="$attrs">
+        {{ name }}
+    </a>
+    <div v-else tabindex="0" class="dropdown dropdown-start dropdown-hover">
+        <a class="btn btn-ghost">
+            {{ name }}
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                stroke-width="1.5" stroke="currentColor" class="size-4 stroke-2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+            </svg>
+        </a>
+        <div
+            class="dropdown-content z-[1] p-4 shadow rounded w-[640px] bg-base-100 hover:bg-base-100">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+<script setup>
+const props = defineProps({
+    name: {
+        type: String,
+        default: ''
+    }
+});
+</script>
